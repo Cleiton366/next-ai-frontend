b58a234c634886a11dea9972bcfa7b35a0e836de
Cleiton <cleiton.queiroz@proton.me>
2024-06-30 22:08:15 -0300

    update: update interface

--- src/interfaces/chat.ts
+++ src/interfaces/chat.ts
@@ -1,9 +1,11 @@
 import Message from "./message";
 
-export default interface Chat {
+interface Chat {
   id: string,
   name: string,
   userId: string,
   messages: Message[]
   createdAt: string
 }
+
+export type { Chat };
