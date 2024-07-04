203a0389d72ddcb097d34978e8ae1d8688126f90
Cleiton <cleiton.queiroz@proton.me>
2024-07-04 15:48:23 -0300

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,41 @@
 'use client';
-import Chat from "@/components/chat";
-import ChatList from "@/components/chat-list";
-import Message from "@/interfaces/message";
-import { useState } from "react";
-import { chats } from '../data'
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { useRouter } from "next/navigation";
+import { FaGoogle } from "react-icons/fa";
 
 export default function Home() {
-  const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(true);
-  const [currentChat, setCurrentChat] = useState<Message[]>(chats[0].messages)
+  const router = useRouter();
 
   return (
-    <main className="flex min-h-screen flex-row items-center justify-between bg-zinc-900" >
-      <ChatList
-        setIsSideBarOpen={setIsSideBarOpen}
-        isSideBarOpen={isSideBarOpen}
-        currentChat={currentChat}
-        setCurrentChat={setCurrentChat}
+    <main className="flex flex-col md:flex-row min-h-screen items-center justify-center bg-black" >
+      <img 
+        src="/logomark.svg" 
+        alt="logo" 
+        className="w-60 h-60 md:w-[30rem] md:h-[30rem]" 
       />
-      <Chat
-        setIsSideBarOpen={setIsSideBarOpen}
-        isSideBarOpen={isSideBarOpen}
-        currentChat={currentChat}
-        setCurrentChat={setCurrentChat}
-      />
+      <div>
+        <div className="flex flex-col items-center">
+          <div className="mb-8 font-semibold md:text-[16pt] text-center">
+            <h1>Your everyday <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#FBFBFB] to-[#8a8dd6]">Open Source</span> Web-UI for</h1>
+            <h1>Your everyday <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#FBFBFB] to-[#8a8dd6]">LLMs</span></h1>
+          </div>
+          <div className="flex flex-col md:flex-row gap-1 md:gap-4">
+            <Button className='bg-accent border-[0.1rem] border-white/15'>
+              <FaGoogle className='md:h-5 md:w-5 mr-2' />
+              <span className='text-[8pt] md:text[14pt]'>Sign in with Google</span>
+            </Button>
+            <Separator className="md:h-10" orientation="vertical" />
+            <Button 
+              className='bg-gradient-to-r from-blue-950 via-accent to-purple-950 border-[0.1rem] w-40 border-none'
+              onClick={() => router.push('/demo')}
+            >
+              <span className='text[14pt]'>View Demo</span>
+            </Button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
