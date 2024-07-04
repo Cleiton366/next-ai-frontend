e72e17676decdaa7f39cf97cb31c2cd2ad516b4a
Cleiton <cleiton.queiroz@proton.me>
2024-07-04 15:53:31 -0300

    update: add router.push to google sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
 
   return (
     <main className="flex flex-col md:flex-row min-h-screen items-center justify-center bg-black" >
-      <img 
-        src="/logomark.svg" 
-        alt="logo" 
-        className="w-60 h-60 md:w-[30rem] md:h-[30rem]" 
+      <img
+        src="/logomark.svg"
+        alt="logo"
+        className="w-60 h-60 md:w-[30rem] md:h-[30rem]"
       />
       <div>
         <div className="flex flex-col items-center">
@@ -22,7 +22,10 @@ export default function Home() {
             <h1>Your everyday <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#FBFBFB] to-[#8a8dd6]">LLMs</span></h1>
           </div>
           <div className="flex flex-col md:flex-row gap-1 md:gap-4">
-            <Button className='bg-accent border-[0.1rem] border-white/15'>
+            <Button
+              className='bg-accent border-[0.1rem] border-white/15'
+              onClick={() => router.push('/chat')}
+            >
               <FaGoogle className='md:h-5 md:w-5 mr-2' />
               <span className='text-[8pt] md:text[14pt]'>Sign in with Google</span>
             </Button>
