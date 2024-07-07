3a1b73aa8ad0de90de39c980efb9eb17d11b9e51
Cleiton <cleiton.queiroz@proton.me>
2024-07-07 18:10:26 -0300

    feat: add user context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { UserProvider } from "@/contexts/user-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,9 @@ export default function RootLayout({
         <link rel="icon" href="./logo.svg" />
       </head>
       <body className={inter.className}>
-        <main>{children}</main>
+        <main>
+          <UserProvider>{children}</UserProvider>
+        </main>
         <Toaster />
       </body>
     </html>
