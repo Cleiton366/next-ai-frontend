d78be4779bafd986169948210e3aab5e6f36dbff
Cleiton <cleiton.queiroz@proton.me>
2024-07-08 01:04:38 -0300

    feat: add chat service

---
+++ src/services/chats-service.ts
@@ -0,0 +1,60 @@
+import { CreateChatDto } from "@/dtos/chat/create-chat-dto";
+import { ChatEntity } from "@/entities/chat/chat-entity";
+
+export default class ChatsServices {
+  private readonly baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+  async getChats(userId: string): Promise<ChatEntity[] | null> {
+    try {
+      const response = await fetch(`${this.baseUrl}/chats/user/${userId}`);
+      const data = await response.json();
+      return data as ChatEntity[];
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async createChat(chat: CreateChatDto): Promise<ChatEntity | null> {
+    try {
+      const response = await fetch(`${this.baseUrl}/chats`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(chat),
+      });
+      const data = await response.json();
+      return data as ChatEntity;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async getChatById(chatId: string): Promise<ChatEntity | null> {
+    try {
+      const response = await fetch(`${this.baseUrl}/chats/${chatId}`);
+      const data = await response.json();
+      return data as ChatEntity;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async deleteChat(chatId: string): Promise<void | null> {
+    try {
+      const response = await fetch(`${this.baseUrl}/chats/${chatId}`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+}
