fe55dd48a93c68f4ee5ea535272745d71030b9d1
Cleiton <cleiton.queiroz@proton.me>
2024-07-09 15:40:50 -0300

    feat: add user preferences service

---
+++ src/dtos/user-preferences/update-user-preferences-dto.ts
@@ -0,0 +1,5 @@
+export class UpdateUsersPreferenceDto {
+  defaultProvider!: string;
+
+  defaultModel!: string;
+}
---
+++ src/services/user-preferences.ts
@@ -0,0 +1,23 @@
+import { UpdateUsersPreferenceDto } from "@/dtos/user-preferences/update-user-preferences-dto";
+import { UserPreferencesEntity } from "@/entities/user-preferences/user-preferences-entity";
+
+export default class MessagesService {
+  private readonly baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+  async updatePreferences(id: string, preferences: UpdateUsersPreferenceDto): Promise<UserPreferencesEntity | null> {
+    try {
+      const response = await fetch(`${this.baseUrl}/user-preferences/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(preferences),
+      });
+      const data = await response.json();
+      return data as UserPreferencesEntity;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+}
